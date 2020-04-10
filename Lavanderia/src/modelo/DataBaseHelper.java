daa334bdd7d7b240cb079bd97de43c9a05fe27b0
Aspharr17 <45318205+Aspharr17@users.noreply.github.com>
2020-04-09 23:00:07 -0500

    Initial Version

---
+++ Lavanderia/src/modelo/DataBaseHelper.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class DataBaseHelper {
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost/lavanderia";
+	private static final String USUARIO = "Aspharr17";
+	private static final String CLAVE = "Ghostrecon17";
+
+	public DataBaseHelper() {
+	}
+
+}
