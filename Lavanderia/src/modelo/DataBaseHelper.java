3c8ed41407524bd64977b1bc525a80c1430daa36
Aspharr17 <aspharr_117@live.com.mx>
2020-04-11 00:58:35 -0500

    Index modificado, vista empezada

--- Lavanderia/src/modelo/DataBaseHelper.java
+++ Lavanderia/src/modelo/DataBaseHelper.java
@@ -2,8 +2,8 @@ package modelo;
 
 public class DataBaseHelper {
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost/lavanderia";
-	private static final String USUARIO = "Aspharr17";
+	private static final String URL = "jdbc:mysql://localhost/lavanderiabd";
+	private static final String USUARIO = "localhost";
 	private static final String CLAVE = "Ghostrecon17";
 
 	public DataBaseHelper() {
